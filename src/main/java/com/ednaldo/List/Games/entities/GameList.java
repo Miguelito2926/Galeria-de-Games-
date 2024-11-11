9301f6b2702cad46fba8f527666fc0281f07d468
Miguelito2926 <ednaldo.junior@prill.com.br>
2024-11-11 19:30:54 -0300

    properties, Game e ORM

---
+++ src/main/java/com/ednaldo/List/Games/entities/GameList.java
@@ -0,0 +1,11 @@
+package com.ednaldo.List.Games.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class GameList {
+}
