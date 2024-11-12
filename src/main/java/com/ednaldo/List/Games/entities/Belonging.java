270948d28ef3ff92373bda8c54248a666cf28b20
Miguelito2926 <ednaldo.junior@prill.com.br>
2024-11-12 10:42:14 -0300

    Domain model

---
+++ src/main/java/com/ednaldo/List/Games/entities/Belonging.java
@@ -0,0 +1,25 @@
+package com.ednaldo.List.Games.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+
+    @EmbeddedId
+    private BelongingPk id = new BelongingPk();
+
+    private Integer position;
+
+    public Belonging(Game game, GameList list, Integer position) {
+        id.setGame(game);
+        id.setList(list);
+        this.position = position;
+    }
+}
--- src/main/java/com/ednaldo/List/Games/entities/GameList.java
+++ src/main/java/com/ednaldo/List/Games/entities/GameList.java
@@ -1,11 +1,21 @@
 package com.ednaldo.List.Games.entities;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class GameList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
 }
